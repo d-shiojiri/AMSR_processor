bd0bbdaed913b336711cba61fde102f9494809b9
Daiya Shiojiri <daiyan3182@gmail.com>
2026-05-26 10:24:47 +0900

    Refacter query_amsr_l3_0p5deg.py

--- query_amsr_l3_0p5deg.py
+++ query_amsr_l3_0p5deg.py
@@ -9,10 +9,7 @@ from typing import Iterator
 import numpy as np
 from netCDF4 import Dataset
 
-try:
-    from .amsr_l3_query import AmsrL3ObservationReader
-except ImportError:
-    from amsr_l3_query import AmsrL3ObservationReader
+from .amsr_l3_query import AmsrL3ObservationReader
 
 
 EPOCH = dt.datetime(1970, 1, 1)
